bfcf08c26c68fe9f032156512b3134862eef102f
Alin Voinea <alin@eaudeweb.ro>
2016-04-22 20:09:21 +0300

    [JENKINS] - Fix version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ ! -z "$BUILDOUT_DEVELOP" ]; then
   GIT=`echo https://github.com/$BUILDOUT_DEVELOP | sed 's/src/eea/g'`
   git clone -v $GIT $BUILDOUT_DEVELOP
   if [ -e "custom.cfg" ]; then
-    echo -e "\n[versions]\n$BUILDOUT_EGGS = >1.0\n" >> custom.cfg
+    echo -e "\n[versions]\n$BUILDOUT_EGGS = >0.9\n" >> custom.cfg
   fi
 fi
 
