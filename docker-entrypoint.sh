3fe4be953ac5b1355ed8aeebd1ea684b0a8d0c09
Alin Voinea <alin.voinea@eaudeweb.ro>
2017-09-08 12:53:03 +0300

    Refs #87909 - Update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 python /docker-initialize.py
 
-if [ ! -z "$BUILDOUT_DEVELOP" ]; then
-  GIT=`echo https://github.com/$BUILDOUT_DEVELOP | sed 's/src/eea/g'`
-  git clone -v $GIT $BUILDOUT_DEVELOP
+if [ ! -z "$DEVELOP" ]; then
+  GIT=`echo https://github.com/$DEVELOP | sed 's/src/eea/g'`
+  git clone -v $GIT $DEVELOP
   if [ -e "custom.cfg" ]; then
-    echo -e "\n[versions]\n$BUILDOUT_EGGS = >0.9\n" >> custom.cfg
+    echo -e "\n[versions]\n$ADDONS = >0.9\n" >> custom.cfg
   fi
 fi
 
