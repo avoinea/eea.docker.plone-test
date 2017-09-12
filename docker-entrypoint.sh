a9c5c4b15f71e070e26a36db9cd55116deb44d31
Alin Voinea <alin.voinea@eaudeweb.ro>
2017-09-12 11:05:39 +0300

    Refs #87909 - Support GIT_BRANCH env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,13 @@ set -e
 
 python /docker-initialize.py
 
+if [ -z "$GIT_BRANCH" ]; then
+  GIT_BRANCH="master"
+fi
+
 if [ ! -z "$DEVELOP" ]; then
   GIT=`echo https://github.com/$DEVELOP | sed 's/src/eea/g'`
-  git clone -v $GIT $DEVELOP
+  git clone -v $GIT --branch=$GIT_BRANCH $DEVELOP
   if [ -e "custom.cfg" ]; then
     echo -e "\n[versions]\n$ADDONS = >0.9\n" >> custom.cfg
   fi
